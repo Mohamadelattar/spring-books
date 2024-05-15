549fc894225071e4738a8f1a14aaea916e2b1a80
Mohamed El attar <mohamadelattar99@gmail.com>
2024-05-15 11:10:53 +0100

    "Spring Boot - GraphQl- Models"

---
+++ src/main/java/com/graphql/books/model/Book.java
@@ -0,0 +1,4 @@
+package com.graphql.books.model;
+
+public record Book(Integer id, String title, Integer pages, Rating rating, Author author) {
+}
---
+++ src/main/java/com/graphql/books/repository/BookRepository.java
@@ -0,0 +1,4 @@
+package com.graphql.books.repository;
+
+public class BookRepository {
+}
